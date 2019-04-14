bc3d79fc6952f5130f56cca3d49054bc8fe958f8
Cory Fitzpatrick <coryfitzpatrick@Corys-MacBook-Pro-2.local>
2019-04-14 14:42:02 -0400

    Update detail pages and about content

--- src/components/detail-item.js
+++ src/components/detail-item.js
@@ -8,34 +8,34 @@ export default class DetailItemContent extends React.Component {
         const category = path.split('/')[1];
         const objectNum = portfolioData[category].findIndex(item => item.url === path);
         const detailItem = portfolioData[category][objectNum];
-        const content = detailItem.subContent;
+        const subContent = detailItem.subContent;
         let subContentLink;
         let subContentDesc;
         let subContentVideo;
 
-        if (detailItem.link) {
-            subContentLink = <a href={content.link} target="_blank" rel="noopener noreferrer">{content.link}</a>;
+        if (subContent.link) {
+            subContentLink = <a href={subContent.link} target="_blank" rel="noopener noreferrer">Visit The Site: {subContent.link}</a>;
         }
 
-        if (detailItem.desc) {
-            subContentDesc = <p>{detailItem.desc}</p>;
+        if (subContent.desc) {
+            subContentDesc = <p>{subContent.desc}</p>;
         }
 
-        if (detailItem.videoLink) {
+        if (subContent.videoLink) {
             subContentVideo = <video preload controls>      
-                 <source src={`/video/${detailItem.videoLink}.webm`} type='video/webm; codecs="vp8, vorbis"' /> 
-                 <source src={`/video/${detailItem.videoLink}.theora.ogv`} type='video/ogg; codecs="theora, vorbis"' /> 
-                 <source src={`/video/${detailItem.videoLink}.mp4`} type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
-                 This web browser does not support HTML5. Upgrade or view the video at <a href="{detailItem.youtubeLink}" title="youtube redirect">{detailItem.youtubeLink}</a>     
+                 <source src={`/video/${subContent.videoLink}.webm`} type='video/webm; codecs="vp8, vorbis"' /> 
+                 <source src={`/video/${subContent.videoLink}.theora.ogv`} type='video/ogg; codecs="theora, vorbis"' /> 
+                 <source src={`/video/${subContent.videoLink}.mp4`} type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+                 This web browser does not support HTML5. Upgrade or view the video at <a href="{subContent.youtubeLink}" title="youtube redirect">{subContent.youtubeLink}</a>     
             </video>;
         }
 
         return (
             <div id="sub-content">
                 <div className="grid-d-12">
-                    <h2>{detailItem.name}</h2>
+                    <h2><a href={subContent.link} target="_blank" rel="noopener noreferrer">{detailItem.name}</a></h2>
 
-                    {content.images.map(image => (
+                    {subContent.images.map(image => (
                         <img key={image} src={image} alt="" />
                     ))}
 
