e2291504f9c90adfb17c25f78827262770b5a3e5
Cory Fitzpatrick <cfitzp15@its.jnj.com>
2025-05-01 18:49:30 -0400

    Go back and try old Procfile new path

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -10,14 +10,14 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get("/web", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "public"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "build"));
 });
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+  res.sendFile(path.join(__dirname, "..", "build/index.html"));
 });
 
 app.listen(port, () => {
