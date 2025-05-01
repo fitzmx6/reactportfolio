97dfacdb2f70a8151a4b325b8cb831db2fe91dec
Cory Fitzpatrick <cfitzp15@its.jnj.com>
2025-05-01 18:20:08 -0400

    Changed root in heroku start

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -10,14 +10,14 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "build"));
+  res.sendFile(path.join(__dirname, "..", "public"));
 });
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "build/index.html"));
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
 app.listen(port, () => {
