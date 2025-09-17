f6a5e2524abcdbbc6c7700d782704e7de1ca5ade
Cory Fitzpatrick <cfitzp15@its.jnj.com>
2025-09-17 14:19:03 -0400

    Use CRA defualt service-worker

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import Router from './components/router';
 import './styles/style.css';
 import './styles/rwdgrid.min.css';
-import registerServiceWorker from './registerServiceWorker';
+import { register } from './serviceWorkerRegistration';
 
 ReactDOM.render(<Router />, document.getElementById('wrapper'));
 
-registerServiceWorker();
+register();
