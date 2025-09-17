15afb8dabd2f67ba20bed18f298558d94b7fb050
Cory Fitzpatrick <cfitzp15@its.jnj.com>
2025-09-17 18:43:01 -0400

    Update about page again

--- src/components/about-page.jsx
+++ src/components/about-page.jsx
@@ -13,7 +13,7 @@ export default class AboutPage extends React.Component {
                     <p>What sets Cory apart is his dedication to mentorship, cross-team alignment, and operational efficiency. He has played a key role in onboarding new customers, supporting production teams, and creating clear documentation to streamline adoption and support. His leadership style emphasizes clarity, accountability, and collaboration.</p>
                     <p>This site was built with <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>, <a href="https://reactjs.org/docs/react-dom.html" target="_blank" rel="noopener noreferrer">ReactDOM 19</a>, <a href="https://reacttraining.com/react-router/web/guides/philosophy" target="_blank" rel="noopener noreferrer">React Router</a>, <a href="https://vite.dev/" target="_blank" rel="noopener noreferrer">Vite</a>, <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3" target="_blank" rel="noopener noreferrer">CSS3</a>, <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5" target="_blank" rel="noopener noreferrer">HTML5</a> and <a href="http://www.rwdgrid.com/" target="_blank" rel="noopener noreferrer">RWDGRID</a>. It is deployed by and hosted on Heroku.</p>
 
-                    <a href="https://github.com/fitzmx6/coryfitzpatrick-frontend" target="_blank" rel="noopener noreferrer">Check Out This Sites Code on Github</a>
+                    <a href="https://github.com/fitzmx6/reactportfolio" target="_blank" rel="noopener noreferrer">Check Out This Sites Code on Github</a>
 
                     <a href="https://www.linkedin.com/in/coryfitzpatrick" target="_blank" rel="noopener noreferrer">Cory’s LinkedIn Profile</a>
                 </div>
