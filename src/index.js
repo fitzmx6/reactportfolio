b2cef95ad4583fcadb13a3d128bd526a6e9f9be0
Cory Fitzpatrick <cfitzp15@its.jnj.com>
2025-09-17 14:36:19 -0400

    Try without service worker

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import Router from './components/router';
 import './styles/style.css';
 import './styles/rwdgrid.min.css';
-import { register } from './serviceWorkerRegistration';
 
 ReactDOM.render(<Router />, document.getElementById('wrapper'));
-
-register();
